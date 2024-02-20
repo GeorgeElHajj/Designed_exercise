b98c3f64916e4e89185330b8b850d7661e3005f6
GeorgeElHajj <georgeselhajj46@gmail.com>
2024-02-20 12:58:47 +0200

    section 1

--- components/SignIn.jsx
+++ components/SignIn.jsx
@@ -4,30 +4,31 @@ export default function SignIn() {
     return (
         <div className=" bg-gg h-auto rounded-bl-3xl w-2/5">
             <nav className="flex flex-row m-10 justify-center mt-8">
-                <ul className="flex flex-row items-center p-0 list-none justify-center space-x-20">
-                    <li className="text-lg font-bold text-white"><Link href="#">Sign in</Link></li>
+                <div className="flex flex-row items-center space-x-20">
+                    <Link href="#" className="text-lg font-bold text-white mr-2">Sign in</Link>
                     <div className="relative py-3 px-4 mb-2">
-                        <li className="text-lg bg-white rounded-xl flex items-center border border-gray-300">
+                        <div className="text-lg bg-white rounded-xl flex items-center border border-gray-300">
                             <img src="sign-in.svg" alt="Sign In Icon" className="h-6 w-6 ml-2" />
                             <Link href="#" className="ml-2 p-2 font-bold">Create Account</Link>
-                        </li>
+                        </div>
                     </div>
-                </ul>
+                </div>
             </nav>
+
             <div className="flex justify-start ml-16">
-            <div className=" bg-white rounded-lg w-16 h-16 flex justify-center items-center transform -rotate-6 mb-6 ">
-                <img src="search1.svg" alt="Sign In Icon" className="h-8 w-8 " />
-            </div>
+                <div className=" bg-white rounded-lg w-16 h-16 flex justify-center items-center transform -rotate-6 mb-6 ">
+                    <img src="search1.svg" alt="Sign In Icon" className="h-8 w-8 " />
+                </div>
             </div>
             <div className="flex justify-end mr-16">
-            <div className=" bg-white rounded-lg w-16 h-16 flex justify-center items-center transform rotate-6 mb-6">
-                <img src="chart.svg" alt="Sign In Icon" className="h-8 w-8" />
-            </div>
+                <div className=" bg-white rounded-lg w-16 h-16 flex justify-center items-center transform rotate-6 mb-6">
+                    <img src="chart.svg" alt="Sign In Icon" className="h-8 w-8" />
+                </div>
             </div>
             <div className="flex justify-start ml-14">
-            <div className=" bg-white rounded-lg w-16 h-16 flex justify-center items-center transform -rotate-6">
-                <img src="shield.svg" alt="Sign In Icon" className="h-8 w-8 " />
-            </div>
+                <div className=" bg-white rounded-lg w-16 h-16 flex justify-center items-center transform -rotate-6">
+                    <img src="shield.svg" alt="Sign In Icon" className="h-8 w-8 " />
+                </div>
             </div>
         </div>
     );
